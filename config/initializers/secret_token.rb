c6dac23e3474f532b085314248a2c6549373a581
HAISHI Takuo <haishi@mynet.co.jp>
2014-10-15 15:09:43 +0900

    Dummy for Deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '137bfcb58fb23baa88e1d94ad17f22372aa4e7903b70fcb3057c72f7d9fe2d62d79d3723a289c2fad9bee3279bfffdda8b166150e47e0207e66f1248cbb41bae'
